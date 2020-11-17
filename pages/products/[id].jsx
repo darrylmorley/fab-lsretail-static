a0e4c29ff60a51cc97ccbfc9e1055b19a7e236c6
Darryl Morley <darrylmorley.uk@gmail.com>
2020-11-17 22:20:51 +0000

    Add error and data states to [id].jsx

--- pages/products/[id].jsx
+++ pages/products/[id].jsx
@@ -13,57 +13,62 @@ const Product = () => {
   const { data, error } = useSWR(`/api/item?itemID=${id}`)
   const { Item } = data
 
+  if (error) return <div>Something went wrong. Try refreshing the page.</div>
 
-  const product = {
-    name: Item.description,
-    description: Item.CustomFieldValues ? Item.CustomFieldValues.CustomFieldValue[1].value : '',
-    sku: Item.customSku,
-    price: Item.Prices.ItemPrice[0].amount.replace('.', ''),
-    currency: 'GBP',
-    image: `${Item.Images.Image.baseImageURL}/w_250/${Item.Images.Image.publicID}.jpg`,
-    itemID: Item.itemID,
-    unitPrice: Item.Prices.ItemPrice[0].amount,
-  }
+  if (!data) return <div>Loading...</div>
 
-  return (
-    <Layout>
-      <div>
+  if (data) {
+    const product = {
+      name: Item.description,
+      description: Item.CustomFieldValues ? Item.CustomFieldValues.CustomFieldValue[1].value : '',
+      sku: Item.customSku,
+      price: Item.Prices.ItemPrice[0].amount.replace('.', ''),
+      currency: 'GBP',
+      image: `${Item.Images.Image.baseImageURL}/w_250/${Item.Images.Image.publicID}.jpg`,
+      itemID: Item.itemID,
+      unitPrice: Item.Prices.ItemPrice[0].amount,
+    }
+
+    return (
+      <Layout>
         <div>
           <div>
-            <Image
-              src={product.image}
-              alt={`Photo of ${product.name}`}
-              width={350}
-              height={350}
-            />
+            <div>
+              <Image
+                src={product.image}
+                alt={`Photo of ${product.name}`}
+                width={350}
+                height={350}
+              />
+            </div>
+            <div>
+              <h1>{product.name}</h1>
+              <p> {formatCurrencyString({
+                value: product.price,
+                currency: product.currency,
+              })}</p>
+
+              <p>{Item.CustomFieldValues && Item.CustomFieldValues.CustomFieldValue[1].value}</p>
+
+              <button
+                onClick={() => addItem(product)}
+                aria-label={`Add ${product.name} to your cart`}
+              >
+                Add to Cart
+            </button>
+
+            </div>
           </div>
-          <div>
-            <h1>{product.name}</h1>
-            <p> {formatCurrencyString({
-              value: product.price,
-              currency: product.currency,
-            })}</p>
-
-            <p>{Item.CustomFieldValues && Item.CustomFieldValues.CustomFieldValue[1].value}</p>
-
-            <button
-              onClick={() => addItem(product)}
-              aria-label={`Add ${product.name} to your cart`}
-            >
-              Add to Cart
-          </button>
-
-          </div>
+          <h3>Full Description</h3>
+          <section>
+            <p>
+              {Item.CustomFieldValues && Item.CustomFieldValues.CustomFieldValue[0].value}
+            </p>
+          </section>
         </div>
-        <h3>Full Description</h3>
-        <section>
-          <p>
-            {Item.CustomFieldValues && Item.CustomFieldValues.CustomFieldValue[0].value}
-          </p>
-        </section>
-      </div>
-    </Layout>
-  )
+      </Layout>
+    )
+  }
 }
 
 export default Product
