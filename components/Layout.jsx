0cb92049de5ec2254577f22e81f7effebb2983d8
Darryl Morley <darrylmorley.uk@gmail.com>
2020-12-21 09:34:25 +0000

    Update tracking code for heap production

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -11,7 +11,7 @@ const Layout = (props) => {
         <script dangerouslySetInnerHTML={{
           __html: `
           window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
-          heap.load("2272009073");
+          heap.load("1521432150");
    `}} />
       </Head>
       <Nav />
